89d0a90b1a33cf485c504b3772162d12752bf01a
warley <warleyroma@gmail.com>
2022-10-22 11:12:37 -0300

    Adicionado o radio button gera o texto do email de chamado fechado

--- scripts/scripts bkp.js
+++ scripts/scripts bkp.js
@@ -65,16 +65,18 @@ const btn = document.getElementById("btn-gerartexto")
 const btn2 = document.getElementById("btn-copiartexto")
 //const btn3 = document.getElementById("btn-apagartexto")
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click",  ()=> {
 
-  const divtxt = document.getElementById("txtemail");
   const radios = document.getElementsByName('gridRadios');
   let inpnumerochamado = document.getElementById('nch').value;
   let inptelefonedousuario = document.getElementById('nusr').value;
   let inpdata = document.getElementById('dttn').value;
   let inphora = document.getElementById('htn').value;
   let string;
+  let strgtent1;
+  let strgchfch;
 
+  const divtxt = document.getElementById("txtemail");
 
   //tratanto o formato que a data vai ser exibida no texto mediante o click no botão
   inpdata = new Date();
@@ -98,79 +100,28 @@ btn.addEventListener("click", () => {
   //lógica para os radio buttons exibirem mudarem uma parte do texto correspondente a tentativa de contato
 
   for (let i = 0; i < radios.length; i++) {
-
     if (radios[i].id === "gridRadios1", "gridRadios2") {
       if (radios[i].checked) {
-        paragrapho1.innerText = `${inpnumerochamado} - `.concat(string).concat(`° aviso de tentativa de contato com o cliente
-        Prezado Cliente,
-        A equipe de Suporte Técnico Remoto informa que não obteve sucesso na `).concat(string).concat(`ª tentativa de contato pelo(s) telefone(s) ${inptelefonedousuario} realizada no dia ${inpdataFormatada} às ${inphora} com o objetivo de iniciar o atendimento da ${inpnumerochamado}.
-        Dessa forma, realizaremos mais uma tentativa de atendimento sem contato telefônico, aplicando procedimento técnico com o objetivo de solucionar a demanda do chamado sem que haja interferência na utilização do seu microcomputador.
-        Eventualmente, poderá ser necessário autorização para acessar a sua área de trabalho. Se for o caso, enviaremos uma solicitação de acesso que aparecerá no centro do seu monitor, conforme janela apresentada abaixo:
-        `)
-
-
-        paragrapho2.innerText = `
-        Neste caso, basta clicar no botão “ACCEPT”, para que o atendimento seja iniciado.
-        Caso não seja possível atender diretamente a demanda sem contato telefônico ou caso não seja respondido ou não autorizado o acesso remoto, esta demanda será concluída sem o devido atendimento, sendo considerado como um chamado improdutivo.
-
-        Atenciosamente,
-        Warleyson Costa Roma `;
-
-        imagem.src = "src/imgpoo.png";
-
-        divtxt.append(paragrapho1);
-        divtxt.append(imagem);
-        divtxt.append(paragrapho2);
-        
-      };
-    };
+        strgtent1 = "mais uma";
+      }
+    }
 
     if (radios[i].id === "gridRadios3") {
       if (radios[i].checked) {
-        paragrapho1.innerText = `${inpnumerochamado} - `.concat(string).concat(`° aviso de tentativa de contato com o cliente
-        Prezado Cliente,
-        A equipe de Suporte Técnico Remoto informa que não obteve sucesso na `).concat(string).concat(`ª tentativa de contato pelo(s) telefone(s) ${inptelefonedousuario} realizada no dia ${inpdataFormatada} às ${inphora} com o objetivo de iniciar o atendimento da ${inpnumerochamado}.
-        Dessa forma, realizaremos a última tentativa de atendimento sem contato telefônico, aplicando procedimento técnico com o objetivo de solucionar a demanda do chamado sem que haja interferência na utilização do seu microcomputador.
-        Eventualmente, poderá ser necessário autorização para acessar a sua área de trabalho. Se for o caso, enviaremos uma solicitação de acesso que aparecerá no centro do seu monitor, conforme janela apresentada abaixo:
-        `)
-
-
-        paragrapho2.innerText =`
-        Neste caso, basta clicar no botão “ACCEPT”, para que o atendimento seja iniciado.
-        Caso não seja possível atender diretamente a demanda sem contato telefônico ou caso não seja respondido ou não autorizado o acesso remoto, esta demanda será concluída sem o devido atendimento, sendo considerado como um chamado improdutivo.
-
-        Atenciosamente,
-        Warleyson Costa Roma `
-
-      };
-    };
-  
+        strgtent1 = "a última";
+        break;
+      }
+    }
 
     if (radios[i].id === "gridRadioschfch") {
       if (radios[i].checked) {
-        paragrapho1.innerText = `${inpnumerochamado} - `.concat(` Chamado Fechado
-        Prezado Cliente,
-        A equipe de Suporte Técnico Remoto informa que após 3 tentativas de contato (por telefone e e-mail) para atendimento da ${inpnumerochamado}, não foi possível proceder com o atendimento do chamado, sendo concluído como improdutivo.
-        
-        Caso ainda necessite de suporte técnico, solicitamos registrar um novo chamado, uma vez que a reabertura desse será considerada indevida.
+        strgchfch = "a última";
+        break;
+      }
+    }
+  }
 
-        Ressaltamos que a equipe de Suporte Técnico preza pela segurança das informações do usuário e por isso adota procedimentos relacionados as tentativas de contato e autorização de usuário para realização do atendimento.
-
-        Com o objetivo de evitarmos outros chamados improdutivos, solicitamos que sejam informados outro(s) número(s) telefônico(s) (se possível, inclusive celular) para que possa ser facilmente encontrado. O(s) e-mail(s) permanecerá(ão) sendo enviado(s) como forma alternada de contato e localização. Desde já agradecemos a compreensão.
-
-        Atenciosamente,
-        Warleyson Costa Roma`)
-
-      
-        paragrapho2.parentNode.removeChild(paragrapho2);
-        imagem.parentNode.removeChild(imagem);
-      };
-    };
-  };
-
-  /**
-  Texto padrão para 1°,2° e 3° tentativas de contato.
-    
+  //Texto padrão para 1°,2° e 3° tentativas de contato. 
   paragrapho1.innerText = `${inpnumerochamado} - `.concat(string).concat(`° aviso de tentativa de contato com o cliente
     Prezado Cliente,
     A equipe de Suporte Técnico Remoto informa que não obteve sucesso na `).concat(string).concat(`ª tentativa de contato pelo(s) telefone(s) ${inptelefonedousuario} realizada no dia ${inpdataFormatada} às ${inphora} com o objetivo de iniciar o atendimento da ${inpnumerochamado}.
@@ -193,25 +144,25 @@ btn.addEventListener("click", () => {
   divtxt.append(paragrapho2);
 
   //console.log(divtxt);
-  */
+
 
   //lógica para botão copiar texto
-  btn2.addEventListener("click", () => {
+  btn2.addEventListener("click", ()=> {
     //let mstnumch = inpnumerochamado;
     let copytxt = divtxt.innerText;
     navigator.clipboard.writeText(copytxt).then(() => {
-
+      
       //alert(`Chamado n° ${mstnumch} copiado para area de transferencia!`);
-    });
+  });
 
   });
 
   //lógica para botão apagar texto
-  document.getElementById('infs').onreset = () => {
+  document.getElementById('infs').onreset = ()=>{
     let clrtext = divtxt;
     clrtext.innerText = "";
-    //return confirm("Gostaria de apagar todas as informações?");
-  };
+      //return confirm("Gostaria de apagar todas as informações?");
+    };
 
 });
 
